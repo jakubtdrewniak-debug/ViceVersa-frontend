23670b53cdf50b8e9f75f295d77566cf2e9db720
Jakub Drewniak <jakub.t.drewniak@gmail.com>
2026-04-23 17:05:05 +0200

    fix tailwind bug

--- viceVersa/vite.config.ts
+++ viceVersa/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import {TanStackRouterVite} from "@tanstack/router-plugin/vite"
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     TanStackRouterVite({
       routesDirectory: "./src/routes",
     }),
