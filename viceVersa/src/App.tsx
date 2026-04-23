7d7b7964865f6dbf1fe5b835354166f62af28442
jakubtdrewniak-debug <jakub.t.drewniak@gmail.com>
2026-04-23 11:22:13 +0200

    add auth0 wrapper and tanstack devtools

--- viceVersa/src/App.tsx
+++ viceVersa/src/App.tsx
@@ -1,11 +1,43 @@
 import './App.css'
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    loginWithRedirect: login,
+    logout: auth0Logout,
+    user,
+  } = useAuth0();
 
-  return (
+  const signup = () =>
+    login({authorizationParams: {screen_hint: "signup"}});
+
+  const logout = () =>
+    auth0Logout({logoutParams: {returnTo: window.location.origin}});
+
+  if (isLoading) return "Loading...";
+
+  return isAuthenticated ? (
     <>
+      <p>Logged in as {user?.email}</p>
+
+      <h1>User Profile</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <button onClick={logout}>Logout</button>
     </>
-  )
+  ) : (
+    <>
+      {error && <p>Error: {error.message}</p>}
+
+      <button onClick={signup}>Signup</button>
+
+      <button onClick={() => login()}>Login</button>
+    </>
+  );
 }
 
 export default App
--- viceVersa/src/main.tsx
+++ viceVersa/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Auth0Provider
+      domain="viceversus.eu.auth0.com"
+      clientId="DusEyIMRSEgOpQjGQ5jM8exZ9FAHV4Oq"
+      authorizationParams={{ redirect_uri: window.location.origin}}>
+          <App />
+      </Auth0Provider>
   </StrictMode>,
 )
